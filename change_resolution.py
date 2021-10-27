465754710dee070195712f9180cf35ad6a6c2a33
himok <himok.pk@gmail.com>
2021-10-27 20:20:53 +0200

    commit1

--- change_resolution.py
+++ change_resolution.py
@@ -1,10 +1,25 @@
 import pygame
-from main import HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT, WIDTH, HEIGHT
-def change_resolution(NEW_WIDTH, NEW_HEIGHT):
+import main
+from main import HEIGHT1
+
+
+def change():
+    global HEIGHT1
+
+    
+    HEIGHT = 100
+    print(HEIGHT1)
+
+
+"""       
+def change_resolution(NEW_WIDTH, NEW_HEIGHT, WIDTH, HEIGHT):
     WIDTH = NEW_WIDTH
     HEIGHT = NEW_HEIGHT
-    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    #WIN = pygame.display.set_mode((NEW_WIDTH, NEW_WIDTH))
 
+    return (NEW_WIDTH, NEW_HEIGHT)
+    
+    
     #menu
     NEW_GAME_BUTTON = pygame.Rect(0.30*WIDTH , 0.35*HEIGHT, 0.40*WIDTH, 0.1*HEIGHT)
     LOAD_GAME_BUTTON = pygame.Rect(0.30*WIDTH , 0.50*HEIGHT, 0.40*WIDTH, 0.1*HEIGHT)
@@ -18,4 +33,4 @@ def change_resolution(NEW_WIDTH, NEW_HEIGHT):
     SFX_SWITCH_RECT = pygame.Rect(0.40*WIDTH, 0.30*HEIGHT, 50, 50)
 
     DEFAULT_PLAYER_X = WIDTH/2 - PLAYER_WIDTH/2
-    DEFAULT_PLAYER_Y = HEIGHT/2 - PLAYER_HEIGHT/2
+    DEFAULT_PLAYER_Y = HEIGHT/2 - PLAYER_HEIGHT/2"""
